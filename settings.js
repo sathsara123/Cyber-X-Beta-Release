95b7c99f6f94e7a66e86936ab60c55ef82f7fee5
sathsara123 <116890440+sathsara123@users.noreply.github.com>
2022-11-10 18:09:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,7 +37,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
 global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.ownername = "ushan " //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
